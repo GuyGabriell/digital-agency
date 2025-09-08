96543b3426c8f6bc01fa99ed877ba007a2f31f27
GuyGabriell <jonahgabriel85@yahoo.com>
2025-09-08 18:51:09 +0100

    ourteamsection

--- src/components/about-us/OurTeam.vue
+++ src/components/about-us/OurTeam.vue
@@ -1,7 +1,53 @@
 <script setup>
+import images from "/public/images/Bill.png";
 
+
+
+defineProps({
+  title: {
+    type: String,
+    default: " Our Team",
+  },
+
+});
 </script>
 
 <template>
-
+  <div>
+    <div>
+      <!-- Title -->
+      <h1
+        class="text-[25px] text-[#222222] font-semibold font-Poppins mb-[px] md:mb-[53px] ml-[px] md:ml-[110px]"
+      >
+       {{ title }}
+      </h1>
+    </div>
+    <div
+      class=""
+    >
+      <!-- Cards -->
+      <div class="grid grid-cols-1 md:grid-cols-4 gap-[24px] md:pl-[110px] md:mr-[70px]">
+        <div class="bg-[#F5F5F5] rounded-lg">
+          <img :src="images" alt="">
+          <p>John peter</p>
+          <span>COO</span>
+        </div>
+        <div class="bg-[#F5F5F5] rounded-lg">
+          <img  src="/public/images/Beverly.png" alt=""> 
+          <p>John peter</p>
+          <span>COO</span> 
+        </div>
+        <div class="bg-[#F5F5F5] rounded-lg">
+          <img src="/public/images/Claudia.png" alt="">
+          <p>John peter</p>
+          <span>COO</span>
+        </div>
+        <div class="bg-[#F5F5F5] rounded-lg">
+          <img src="/public/images/Avatar.png" alt="">
+          <p>John peter</p>
+          <span>COO</span>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
--- src/pages/About.vue
+++ src/pages/About.vue
@@ -2,6 +2,7 @@
 import HeroAbout from "@/components/about-us/HeroAbout.vue";
 import WorkWithUs from "@/components/about-us/WorkWithUs.vue";
 import IntroAbout from "@/components/about-us/IntroAbout.vue";
+import OurTeam from "@/components/about-us/OurTeam.vue";
 
 
 </script>
@@ -11,6 +12,7 @@ import IntroAbout from "@/components/about-us/IntroAbout.vue";
     <HeroAbout />
     <WorkWithUs />
     <IntroAbout />
+    <OurTeam />
     
   </div>
 </template>
